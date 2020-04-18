ac2bbaa13ac044bf21a38b47802f39f12aace3aa
Miguel S. Navarro <miguels.navarro@MacBook-Pro-de-Miguel.local>
2020-04-18 20:01:29 -0300

    Second commit... practising

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class label extends React.Component {}
+
 const App = () => {
-  return <div>Hi there!!!</div>;
+  //function getButtonText() {
+  // return 'Click me!!!';
+  //};
+
+  var texto = {
+    name: 'Click me!!',
+  };
+
+  const style = {
+    backgroundColor: 'blue',
+    color: 'white',
+  };
+
+  return (
+    <div style={style}>
+      <label className="label" htmlFor="name">
+        Enter name
+      </label>
+      <input id="name" type="text" />
+
+      <button>{texto.name}</button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
